bcc814ffebd9662995c253b574b2c4368e56a03d
ANKIT KUMAR <ankitkumarrxl909@gmail.com>
2024-12-06 23:08:33 +0530

    complete

--- oldCode/main.py
+++
@@ -1,8 +0,0 @@
-import recorded2, program
-
-cmd = int(input("Press 1 for Live caputre \n \t OR \nPress 2 to caputre from recorded video"))
-
-if cmd == 1:
-    program.main()
-else:
-    recorded2.func1()
